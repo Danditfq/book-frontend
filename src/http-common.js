29c086194d88e1652a69058d508d70cce9f8e8a8
danditfq <danditaufiq@gmail.com>
2022-10-24 21:50:05 +0700

    commit lagi

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "internal-alb-app-dandi-1122638326.ap-southeast-2.elb.amazonaws.com/api",
   headers: {
     "Content-type": "application/json"
   }
