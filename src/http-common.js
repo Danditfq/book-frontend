699a76e43e6416330d683979d912201f5ad44175
danditfq <danditaufiq@gmail.com>
2022-10-27 10:35:54 +0700

    test

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "internal-alb-app-dandi-1122638326.ap-southeast-2.elb.amazonaws.com/api",
+  baseURL: "https://dandi-backend.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
 });
+
